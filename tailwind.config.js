706303b8c628a28eca6133616c14bad8570998dd
Moinul Moin <moinulmoin101@gmail.com>
2021-11-22 01:37:50 +0600

    added navbar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	mode: 'jit',
+	purge: ['./*.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		container: {
+			center: true,
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
